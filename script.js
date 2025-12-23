ced031d0621e3999e0d2fa3ead31cf830260f7c3
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-12-23 13:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i = 1 ; i<str.length-1;i++){
+		if(str[i]!==str[i+1] && str[i-1]!==str[i])
+			 return str[i]
+	}
+	if(str[str.length-1]!==str[str.length-2])
+		return str[str.length-1 ]
+	return null
 }
-const input = prompt("Enter a string");
+  const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
